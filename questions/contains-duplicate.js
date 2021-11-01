956271d808b55d25ddd65faf94bc19474a34657e
Dmitriy Zhiganov <35735722+delawere@users.noreply.github.com>
2021-11-01 16:23:56 +0300

    Update contains-duplicate.js

--- questions/contains-duplicate.js
+++ questions/contains-duplicate.js
@@ -3,7 +3,7 @@
  * @return {boolean}
  */
 
-/** Problem has many solutions. The shortest is solution with Set. 
+/** Problem has many solutions. The shortest solution is solution with Set. 
  * Set contains only unique elements, so we create new Set from array 
  * and compare their lengths.
  * If they have same lengths, array doesn't contain duplicates.
